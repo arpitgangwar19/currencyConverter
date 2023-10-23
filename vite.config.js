8024ee83001e1e154b9ba1c84f144632d7c6f5de
arpitgangwar <arpitsingh414@gmail.com>
2023-10-23 20:45:36 +0530

    build run

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 export default defineConfig({
+  base: '/my-react-app/', // Adjust based on your deployment path
   plugins: [react()],
 })
